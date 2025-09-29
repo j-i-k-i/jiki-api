56ac6a6b21cbc2fe2ed90c55eb72e39969c0d74d
Jeremy Walker <jez.walker@gmail.com>
2025-09-29 16:13:26 +0200

    Tweak pattern

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,8 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable,
     :jwt_authenticatable, jwt_revocation_strategy: self
 
-  # Generate a unique JTI (JWT ID) for each user on creation
-  before_create :generate_jti
-
-  private
-  def generate_jti
+  before_create do
+    # Generate a unique JTI (JWT ID) for each user on creation
     self.jti = SecureRandom.uuid
   end
 end
